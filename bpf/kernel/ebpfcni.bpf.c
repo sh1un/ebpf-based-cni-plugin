b362eed6d7d2f5e76c373db6cad594c38ae59d1b
sh1un <a0923183408@gmail.com>
2025-09-11 11:40:20 +0800

    refactor: simplify TC parser by removing unused maps and streamlining logic

--- bpf/kernel/ebpfcni.bpf.c
+++ bpf/kernel/ebpfcni.bpf.c
@@ -1,93 +1,38 @@
 #include <linux/bpf.h>
-#include <linux/in.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
 #include <linux/pkt_cls.h>
 #include <bpf_helpers.h>
 #include <bpf_endian.h>
 
-#define ETH_P_IP 0x0800
-#define MAX_ENTRIES 1000
-
-struct
-{
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, MAX_ENTRIES);
-    __uint(key_size, sizeof(__u32));
-    __uint(value_size, sizeof(__u64));
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
-} counter SEC(".maps");
-
-struct ip_pair
-{
-    __u32 saddr;
-    __u32 daddr;
-};
-
-struct
-{
-    __uint(type, BPF_MAP_TYPE_HASH);
-    __uint(max_entries, MAX_ENTRIES);
-    __type(key, struct ip_pair);
-    __type(value, int);
-    __uint(pinning, LIBBPF_PIN_BY_NAME);
-} iprules SEC(".maps");
-
-SEC("classifier")
-int process_tc(struct __sk_buff *skb)
-{
-    void *data = (void *)(long)skb->data;
-    void *data_end = (void *)(long)skb->data_end;
-    struct ethhdr *eth = data;
-    if (data + sizeof(struct ethhdr) > data_end)
-        return TC_ACT_SHOT;
-    if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
-        return TC_ACT_OK;
-
-    struct iphdr *iph = data + sizeof(struct ethhdr);
-    if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end)
-        // If not a valid iph, we abort.
-        return TC_ACT_SHOT;
-
-    bpf_printk("Got TCP packet from %x", bpf_ntohl(iph->saddr));
-    bpf_printk("Got TCP packet to %x", bpf_ntohl(iph->daddr));
-
-    __u32 src_ip_key = iph->saddr;
-
-    __u64 *value = bpf_map_lookup_elem(&counter, &src_ip_key);
-    if (value)
-    {
-        (*value)++;
-    }
-    else
-    {
-        __u64 one = 1;
-        bpf_map_update_elem(&counter, &src_ip_key, &one, BPF_NOEXIST);
-    }
-
-    // Logic for blocking traffic conditionally
-
-    // The key is the pair source-addr IP and the value is a boolean 0/no allow 1/allowed
-    struct ip_pair ip_pair_key;
-    ip_pair_key.saddr = bpf_ntohl(iph->saddr);
-    ip_pair_key.daddr = bpf_ntohl(iph->daddr);
-
-    int *value_ip_pair = bpf_map_lookup_elem(&iprules, &ip_pair_key);
-    if (value_ip_pair)
-    {
-        // Rule exists, check if it's an allow (1) or deny (0) rule
-        if (*value_ip_pair == 1)
-        {
-            return TC_ACT_OK; // Allow
-        }
-        else
-        {
-            return TC_ACT_SHOT; // Deny
-        }
-    }
-
-    // Default action: if no rule is found, drop the packet.
-    // The controller is responsible for adding rules.
-    return TC_ACT_SHOT;
-}
 char _license[] SEC("license") = "GPL";
+
+// 定義 TC 程式的 section
+SEC("tc")
+int simple_tc_parser(struct __sk_buff *skb)
+{
+    // 取得封包資料的起始與結束位置
+    void *data_end = (void *)(long)skb->data_end;
+    void *data = (void *)(long)skb->data;
+    struct ethhdr *eth = data;
+    struct iphdr *iph = data + sizeof(*eth);
+
+    // 檢查邊界
+    if ((void *)iph + sizeof(*iph) > data_end)
+    {
+        return TC_ACT_SHOT;
+    }
+
+    // 檢查是否為 IPv4 封包
+    if (eth->h_proto != bpf_htons(ETH_P_IP))
+    {
+        return TC_ACT_OK; // 不是 IPv4，放行
+    }
+
+    // 印出封包資訊
+    bpf_printk("TC: Got a packet from 0x%x to 0x%x",
+               bpf_ntohl(iph->saddr), bpf_ntohl(iph->daddr));
+
+    // 放行封包
+    return TC_ACT_OK;
+}
